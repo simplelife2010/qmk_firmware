c29d52bcbcfdfea9047eef72f494ca8061a6f345
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-01-24 14:56:43 +0100

    Fix symbols

--- keyboards/kyria/keymaps/simplelife2010/keymap.c
+++ keyboards/kyria/keymaps/simplelife2010/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_LOWER] = LAYOUT(
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-      _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,                                     _______, _______, _______, _______, _______, KC_PIPE,
+      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,                                     _______, _______, _______, _______, _______, KC_PIPE,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_RAISE] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_BSLS,
+      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_BSLS,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
