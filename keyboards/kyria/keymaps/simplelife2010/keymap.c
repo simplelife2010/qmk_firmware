513c4d95e6380373d538bb280bd3a53f4d15e4c2
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-01-31 14:57:47 +0100

    Home row mods

--- keyboards/kyria/keymaps/simplelife2010/keymap.c
+++ keyboards/kyria/keymaps/simplelife2010/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,---------------------------------------------.
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |   Bksp   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+----------|
- * |  Esc   |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |   ' "    |
+ * |  Esc   |A/Ctrl|S/Alt |D/GUI |F/Sft |   G  |                              |   H  |J/Sft |K/GUI |L/Alt |;/Ctrl|   ' "    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+----------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | Alt  |      |  |      | Alt  |   N  |   M  | ,  < | . >  | /  ? | RS/Enter |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+------------------------'
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,                                             KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_ESC,  KC_A,   KC_S,   KC_D,   KC_F,    KC_G,                                             KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,       KC_LALT,  KC_NO,   KC_NO,   KC_RALT,  KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-                               KC_NO,  KC_LGUI, MO(_LOWER), KC_SPC,   KC_LCTL, KC_RCTL, KC_SPC,   MO(_RAISE),  KC_RGUI, KC_NO
+      KC_TAB,  KC_Q        , KC_W,         KC_E,         KC_R,         KC_T,                                           KC_Y,        KC_U,         KC_I,         KC_O,         KC_P,            KC_BSPC,
+      KC_ESC,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,                                           KC_H,        LSFT_T(KC_J), LGUI_T(KC_K), LALT_T(KC_L), LCTL_T(KC_SCLN), KC_QUOT,
+      KC_LSFT, KC_Z        , KC_X,         KC_C,         KC_V,         KC_B,       KC_LALT, KC_NO,   KC_NO,   KC_RALT, KC_N,        KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_SFTENT,
+                                           KC_NO,        KC_LGUI,      MO(_LOWER), KC_SPC,  KC_LCTL, KC_RCTL, KC_SPC,  MO(_RAISE),  KC_RGUI,      KC_NO
     ),
 /*
  * Lower Layer: Symbols
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
       _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,                                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, KC_PIPE,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN2, KC_BTN1,
+                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 /*
  * Raise Layer: Number keys, navigation
