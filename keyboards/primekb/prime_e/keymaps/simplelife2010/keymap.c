2dfd78b66d808ad2f48abf29f30d9a36ca3edc4a
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-09-19 15:14:10 +0200

    Keymap for Prime Elise

--- keyboards/primekb/prime_e/keymaps/simplelife2010/keymap.c
+++ keyboards/primekb/prime_e/keymaps/simplelife2010/keymap.c
@@ -21,7 +21,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
 		KC_ESC,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,    KC_DEL, KC_BSPC,
-		HYP_TAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_QUOT, KC_ENT,
+		HYP_TAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN, KC_ENT,
 		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          MO(3),  KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 		KC_LCTL,  KC_LALT,                    KC_LCMD, MO(1),         L2_SPC, KC_RCMD,                          KC_RALT, KC_RCTL
     ),
