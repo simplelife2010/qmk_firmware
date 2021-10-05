33c0b7100263af1a7a08d2aef461d086ac94cbf5
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-10-05 17:16:25 +0200

    Fix planck

--- keyboards/planck/keymaps/simplelife2010/keymap.c
+++ keyboards/planck/keymaps/simplelife2010/keymap.c
@@ -8,34 +8,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _QWERTY_ALPHAS1_12_,
         _QWERTY_ALPHAS2_12_,
         _QWERTY_ALPHAS3_12_,
-        _QWERTY_ROW4_40PERCENT_4MODS_2SPC_ 
+        _QWERTY_BTM_4MODS_2SPC_
     ),
 
     [_LOWER] = LAYOUT_planck_2x2u_wrapper(
         _LOWER_ALPHAS1_12_,
         _LOWER_ALPHAS2_12_,
         _LOWER_ALPHAS3_12_,
-        _LOWER_ROW4_40PERCENT_4MODS_2SPC_ 
+        _LOWER_BTM_4MODS_2SPC_
     ),
 
     [_RAISE] = LAYOUT_planck_2x2u_wrapper(
         _RAISE_ALPHAS1_12_,
         _RAISE_ALPHAS2_12_,
         _RAISE_ALPHAS3_12_,
-        _RAISE_ROW4_40PERCENT_4MODS_2SPC_ 
+        _RAISE_BTM_4MODS_2SPC_
     ),
 
     [_ADJUST] = LAYOUT_planck_2x2u_wrapper(
         _ADJUST_ALPHAS1_12_,
         _ADJUST_ALPHAS2_12_,
         _ADJUST_ALPHAS3_12_,
-        _ADJUST_ROW4_40PERCENT_4MODS_2SPC_ 
+        _ADJUST_BTM_4MODS_2SPC_
     ),
 
     [_MOUSE] = LAYOUT_planck_2x2u_wrapper(
         _MOUSE_ALPHAS1_12_,
         _MOUSE_ALPHAS2_12_,
         _MOUSE_ALPHAS3_12_,
-        _MOUSE_ROW4_40PERCENT_4MODS_2SPC_ 
+        _MOUSE_BTM_4MODS_2SPC_
     )
+
 };
